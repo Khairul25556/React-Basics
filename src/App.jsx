6ab9722548df7195bdeef100a059ea89d43e568a
Khairul25556 <krft44@gmail.com>
2025-05-17 22:20:44 +0600

    [Day 2] implemented props, array passing, conditional rendering, and dynamic styling in React components. Fixes #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css"
-import Title from "./Title.jsx";
 import ProductTab from "./ProductTab.jsx"; 
 
 
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,10 +1,17 @@
 import "./Product.css"
 
-function Product() {
+function Product({title, price=1000, feature1 = [], feature2= {}}) {
+    // let list = feature1.map((feature1)=><li>{feature1}</li>);
+    let isDiscount = price >= 30000; 
+    let styles = {backgroundColor: isDiscount ? "red" : null};
+
     return(
-    <div className="Product">
-        <h3>Product Name</h3>
-        <h5>Product Description</h5>
+    <div className="Product" style={styles}>
+        <h3>{title}</h3>
+        <h5>Price: {price}</h5>
+        {isDiscount ? <p>Discount of 5%</p> : null}
+        {/* <p>{feature1.map((feature1)=><li>{feature1}</li>)}</p>
+        <p>{feature2.b}</p> */}
     </div>
     );
 }
